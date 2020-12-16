a1281796c6ece5f01f9d74dc00d6d293b8a45968
Stanis1av <krasavin.stanislaus@gmail.com>
2020-12-16 16:48:34 +0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'welcome#index'
 
-  # resources :bookings, :only [:new, :create]
-
-  end
+  resource :bookinkg, only: [:new, :create, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
