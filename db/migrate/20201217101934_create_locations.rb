a06e21e3e3c9bf8b774b7520bcd9c2c2a52d76ed
Stanis1av <krasavin.stanislaus@gmail.com>
2020-12-17 17:00:00 +0300

    Update location migration

---
+++ db/migrate/20201217101934_create_locations.rb
@@ -0,0 +1,13 @@
+class CreateLocations < ActiveRecord::Migration[6.0]
+  def change
+    create_table :locations do |t|
+      t.string :city
+      t.string :name_location
+      t.string :address
+      t.decimal :latitude, precision: 10, scale: 6
+      t.decimal :longitude, precision: 10, scale: 6
+
+      t.timestamps
+    end
+  end
+end
