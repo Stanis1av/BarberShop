a93964be9998ce8cb54937ba9a26d215fb4985dd
Stanis1av <krasavin.stanislaus@gmail.com>
2020-12-17 23:21:01 +0400

    Trying to add Google Map API

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,8 @@
 class Location < ApplicationRecord
+  geocoded_by :address
+  after_validation :geocode, if: :address_changed?
+
+  def google_map(center)
+    "https://maps.googleapis.com/maps/api/staticmap?center=#{center}&size=500x500&zoom=17&key=#{Rails.application.credentials.name_you_choose}"
+end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Location.create!([
+{ "address": "Buckingham Palace", "latitude": "51.501564","longitude": "-0.141944"},
+{ "address": "Westminster Abbey", "latitude": "51.499581", "longitude": "-0.127309"},
+{ "address": "Big Ben", "latitude": "51.500792", "longitude": "-0.124613"}
+])
