97d55a31298333f0918372358c8c69e520d4de1d
Stanis1av <krasavin.stanislaus@gmail.com>
2020-12-21 10:29:29 +0300

    Update app

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,13 +1,19 @@
 class BookingsController < ApplicationController
+  def show
+    @booking = Booking.all
+  end
+
   def new
 
   end
 
   def create
     @booking  = Booking.new(booking_params)
+    @booking.hairdresser = params[:hairdresser]
+    @booking.dateandtime = params[:dateandtime]
 
     if @booking.save
-      redirect_to @booking
+      redirect_to bookings_url
     else
       render action: 'new'
     end
@@ -16,6 +22,6 @@ class BookingsController < ApplicationController
   private
 
   def booking_params
-    params.require(:booking).permit(:first_name, :last_name, :hairdresser, :dateandtime, :phone_num)
+    params.require(:booking).permit(:first_name, :last_name, :hairdresser, :dateandtime, :phone_num, :note)
   end
 end
