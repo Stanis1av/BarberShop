a2f3d4ee9ef78b4e3c345c22ccd1ff10584fd553
Stanis1av <krasavin.stanislaus@gmail.com>
2021-02-19 20:06:38 +0300

    Create model Locations and update migration Booking

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,2 +1,4 @@
 class Booking < ApplicationRecord
+  has_one :location
+  accepts_nesteed_attributes_for :location
 end
--- db/migrate/20201219181055_create_bookings.rb
+++ db/migrate/20201219181055_create_bookings.rb
@@ -1,13 +1,11 @@
 class CreateBookings < ActiveRecord::Migration[6.0]
   def change
     create_table :bookings do |t|
+      t.reference :locations, null: false, foreign_key: true
+      #t.reference :hairdressers, null: false, foreign_key: true
+      #t.reference :services, null: false, foreign_key: true
       t.string :first_name
       t.string :last_name
-      t.reference :service, null: false, foreign_key: true
-      t.string :location, null: false, foreign_key: true
-      t.reference :service, null: false, foreign_key: true
-      t.string :hairdresser
-      t.references :service
       t.datetime :dateandtime
       t.string :phone_num
       t.text :note
