f9f311383f978a67ecaffb3e53e34edec47201ea
Stanis1av <krasavin.stanislaus@gmail.com>
2021-02-25 01:48:27 +0300

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_22_192241) do
+ActiveRecord::Schema.define(version: 2021_02_24_165050) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,11 +35,6 @@ ActiveRecord::Schema.define(version: 2021_02_22_192241) do
   create_table "contacts", force: :cascade do |t|
     t.string "email"
     t.text "message"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "dashboards", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
