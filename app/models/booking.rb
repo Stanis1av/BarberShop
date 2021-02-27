abc0870cade475f8115107b34503fa528ec909af
Stanis1av <krasavin.stanislaus@gmail.com>
2021-02-27 19:17:18 +0300

    Add associations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,8 @@
 class Booking < ApplicationRecord
-  # has_one :location # , inverse_of: :booking
-  # accepts_nested_attributes_for :location
+  has_one :location # , inverse_of: :booking
+  accepts_nested_attributes_for :location
+  has_one :service # , inverse_of: :booking
+  accepts_nested_attributes_for :service
+  has_one :hairdresser # , inverse_of: :booking
+  accepts_nested_attributes_for :hairdresser
 end
