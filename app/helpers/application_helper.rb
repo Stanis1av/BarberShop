bd53c9ccff8a8a2f97f24de93f5be1d4aa9b78de
Stanis1av <krasavin.stanislaus@gmail.com>
2021-02-28 03:59:33 +0300

    Change link for registration

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,14 @@
 module ApplicationHelper
-  def resource_name
-     :user
-  end
+  # if new_registration_path(resource_name)
+  # def resource_name
+  #    :user
+  # end
 
-  def resource
-     @resource ||= User.new
-  end
+  # def resource
+  #    @resource ||= User.new
+  # end
 
-  def devise_mapping
-     @devise_mapping ||= Devise.mappings[:user]
-  end
+  # def devise_mapping
+  #    @devise_mapping ||= Devise.mappings[:user]
+  # end
 end
