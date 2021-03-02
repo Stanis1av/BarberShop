544592b09befea4ea9643dfc164909b9f00ab3df
Stanis1av <krasavin.stanislaus@gmail.com>
2021-03-03 00:10:31 +0300

    Create new resources to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,11 @@ Rails.application.routes.draw do
   resource :bookings, only: [:new, :create, :show]
   resource :contacts, only: [:new, :create, :show]
   resource :abouts, only: [:show],  path_names: { :show => '' }
+
+  namespace :admin do
+    get '/', to: 'dashboard#index', as: :dashboard
+    resources :bookings, only: [:index, :show, :update]
+    # resources :users, only: [:index, :show]
+  end
   # For details on the DSL available within this fileshow see https://guides.rubyonrails.org/routing.html
 end
