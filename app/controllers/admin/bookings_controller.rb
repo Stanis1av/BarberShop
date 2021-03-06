2b60f544c1c07469a61f0f55b2e594ee9604d0f4
Stanis1av <krasavin.stanislaus@gmail.com>
2021-03-06 20:04:41 +0300

    Update admin/bookings controller

--- app/controllers/admin/bookings_controller.rb
+++ app/controllers/admin/bookings_controller.rb
@@ -3,15 +3,52 @@ class Admin::BookingsController < Admin::BaseController
     @bookings = Booking.all
   end
 
+  def new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+
+    if @booking.save
+      redirect_to @booking
+    else
+      render action: 'new'
+    end
+  end
+
   def show
+    @booking = Booking.find(params[:id])
   end
 
   def edit
+    @booking = Booking.find(params[:id])
   end
 
   def update
+    @booking = Booking.find(params[:id])
+
+    if @booking.update(booking_params)
+      redirect_to @booking
+    else
+      render action: 'edit'
+    end
   end
 
   def destroy
+    @booking = Booking.find(params[:id])
+
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:location_id,
+                                    :service_id,
+                                    :hairdresser_id,
+                                    :first_name,
+                                    :last_name,
+                                    :dateandtime,
+                                    :phone_num,
+                                    :note)
   end
 end
