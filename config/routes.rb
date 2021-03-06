33c53e15d2b248d93642687f9391ae3031aeef9b
Stanis1av <krasavin.stanislaus@gmail.com>
2021-03-06 22:52:46 +0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
 
   namespace :admin do
     get '/', to: 'dashboard#index', as: :dashboard
-    resources :bookings, only: [:index, :show, :update]
+    resources :bookings#, only: [:index, :show, :update]
     # resources :users, only: [:index, :show]
   end
   # For details on the DSL available within this fileshow see https://guides.rubyonrails.org/routing.html
