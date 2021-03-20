299d033ec021f93f391745ed38742e820f0210d5
Stanis1av <krasavin.stanislaus@gmail.com>
2021-03-20 11:31:39 +0300

    Add check for empty value (last.updated_at)

--- app/helpers/admin/dashboard_helper.rb
+++ app/helpers/admin/dashboard_helper.rb
@@ -1,2 +1,17 @@
 module Admin::DashboardHelper
+  def booking_last_update
+    if @bookings.empty?
+      0
+    else
+      @bookings.last.updated_at
+    end
+  end
+
+  def user_last_update
+    if @users.empty?
+      0
+    else
+      @users.last.updated_at
+    end
+  end
 end
