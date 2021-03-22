861cf1dcbdcc71bdd31babc01d3f2b4c371686f5
Stanis1av <krasavin.stanislaus@gmail.com>
2021-03-22 11:08:57 +0300

    Add trackable

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -4,7 +4,15 @@ class Admin::UsersController < Admin::BaseController
   end
 
   def show
-    @users = User.find(params[:id])
+    @user = User.find(params[:id])
+
+    @joined_on = @user.created_at.to_formatted_s(:short)
+
+    if @user.current_sign_in_at
+      @last_login = @user.current_sign_in_at.to_formatted_s(:short)
+    else
+      @last_login = 'never'
+    end
   end
 
   def new
--- db/migrate/20201211212412_add_username.rb
+++
@@ -1,5 +0,0 @@
-class AddUsername < ActiveRecord::Migration[6.0]
-  def change
-
-  end
-end
