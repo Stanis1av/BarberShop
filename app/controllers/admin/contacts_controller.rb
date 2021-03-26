e33403a2b07549188a1f7b83a31880c71779e226
Stanis1av <krasavin.stanislaus@gmail.com>
2021-03-26 13:12:15 +0400

    Update controller admin/contacts

--- app/controllers/admin/contacts_controller.rb
+++ app/controllers/admin/contacts_controller.rb
@@ -26,24 +26,32 @@ class Admin::ContactsController < Admin::BaseController
 
   def edit
     @contact = Contact.find(params[:id])
-
-    if @contact.valid? && @contact.save?
-      redirect_to admin_contacts_path, notice: 'Сообщение успешно отредактированно'
-    else
-      render action: 'new', alert: 'Не удалось отредактировать сообщение'
-    end
   end
 
   def update
+    @contact = Contact.find(params[:id])
+
+    if @contact.valid? && @contact.update?
+      redirect_to admin_contacts_path, notice: 'Сообщение успешно отредактированно'
+    else
+      render action: 'edit', alert: 'Не удалось отредактировать сообщение'
+    end
   end
 
   def destroy
+    @contact = Contact.find(params[:id])
+
+    if @contact.destroy?
+      redirect_to admin_contacts_path
+    else
+      redirect_to admin_contacts_path
+    end
   end
 
   private
 
   def contact_params
-    params.require(:contact).permit(
-      )
+    params.require(:contact).permit(:email,
+                                    :message)
   end
 end
